ad381aaf1b5135d86e17a49894b82736727e143a
Pham Cao Hoang An <pchoangan@gmail.com>
2025-06-03 14:00:24 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-// app/page.js
-// Đây là một Server Component theo mặc định
+import React, { useState, useEffect, useMemo } from "react";
+
 async function getData() {
   const res = await fetch('https://script.google.com/macros/s/AKfycbxGGPAp9NDCHtOVdPHfAnnxBbjB9TSGpJeyB1QPsNlbmpD3FYxuG3cx3auIr9bOFFnl/exec');
   if (!res.ok) {
@@ -9,11 +9,16 @@ async function getData() {
 }
 
 export default async function HomePage() {
-  const data = await getData(); // Fetch dữ liệu trên server
+  const [dataTable, setDataTable] = useState([]);
+  useEffect(() => {
+    const data = await getData(); // Fetch dữ liệu trên server
+    setDataTable(data);
+  }, []);
+  
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
-      <p>Data from server:{JSON.stringify(data)||"ok"} </p>
+      <p>Data from server:{JSON.stringify(dataTable)||"ok"} </p>
     </main>
   );
 }
