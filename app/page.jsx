cfe105527e63609f563d17d79f58d4496ef61464
Pham Cao Hoang An <pchoangan@gmail.com>
2025-06-03 20:46:03 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,68 @@
-
 "use client";
 import React, { useState, useEffect, useMemo } from "react";
 
-export default function HomePage() { 
-  const [dataTable, setDataTable] = useState([]);  
+export default function HomePage() {
+  const [dataTable, setDataTable] = useState([]);
+  const [filterText, setFilterText] = useState("");
 
   useEffect(() => {
     async function fetchData() {
-      try {        
-        const response = await fetch('https://script.google.com/macros/s/AKfycbxGGPAp9NDCHtOVdPHfAnnxBbjB9TSGpJeyB1QPsNlbmpD3FYxuG3cx3auIr9bOFFnl/exec');
+      try {
+        const response = await fetch(
+          "https://script.google.com/macros/s/AKfycbxGGPAp9NDCHtOVdPHfAnnxBbjB9TSGpJeyB1QPsNlbmpD3FYxuG3cx3auIr9bOFFnl/exec"
+        );
         const data = await response.json();
         setDataTable(data);
       } catch (err) {
-        console.error(err); // Log lỗi ra console
-      } 
+        console.error(err);
+      }
     }
     fetchData();
-  }, []); // Mảng rỗng đảm bảo useEffect chỉ chạy một lần sau khi component được mount
-  
+  }, []);
+
+  const filteredData = useMemo(() => {
+    if (dataTable.length > 0) {
+      const header = dataTable[0];
+      const data = dataTable.slice(1);
+
+      const filtered = data.filter((row) => {
+        return row.some((cell) =>
+          String(cell).toLowerCase().includes(filterText.toLowerCase())
+        );
+      });
+      return [header, ...filtered];
+    } else {
+      return [];
+    }
+  }, [dataTable, filterText]);
+
+  const handleFilterChange = (e) => {
+    setFilterText(e.target.value);
+  };
+
   return (
     <main>
+      <div>
+        <input
+          type="text"
+          placeholder="Filter..."
+          value={filterText}
+          onChange={handleFilterChange}
+        />
+      </div>
       <div id="table-container">
+        {filteredData.length > 0 && (
           <table id="myTable">
             <thead>
               <tr>
-                {dataTable[0].map((header, index) => (
+                {filteredData[0].map((header, index) => (
                   <th key={index}>{header}</th>
                 ))}
               </tr>
             </thead>
             <tbody>
-              {dataTable.slice(1).map((rowData, rowIndex) => (
-                <tr
-                  key={rowIndex}
-                >
+              {filteredData.slice(1).map((rowData, rowIndex) => (
+                <tr key={rowIndex}>
                   {rowData.map((cellData, cellIndex) => (
                     <td key={cellIndex}>
                       <div className="cell">{cellData}</div>
@@ -43,7 +72,8 @@ export default function HomePage() {
               ))}
             </tbody>
           </table>
-        </div>
+        )}
+      </div>
     </main>
   );
 }
